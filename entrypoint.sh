f8010cf0e62a3106796342c34fa859491e3af165
apardo04 <apardo04@gmail.com>
2020-05-21 14:18:32 -0400

    editing bash script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,13 @@
 
 # `$*` expands the `args` supplied in an `array` individually 
 # or splits `args` in a string separated by whitespace.
-if [ -z ${locustfile+x} ]; then
+if [ -z ${locustfile} ]; then
     file = "/locustfile.py"
 else
     file = "/github/workspaces/$locustfile"
 fi
 
 echo locustfile variable = $file
+locust --version
 
 sh -c "locust -f $file --headless -u 5 -r 5 --run-time 10s -H $URL"
