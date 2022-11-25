2dac04a2bab942b17b7b078acc2c1b4fc63544dd
apardo04 <apardo04@gmail.com>
2022-11-25 13:24:39 -0500

    testing test.yml

--- locustfile.py
+++ locustfile.py
@@ -2,8 +2,10 @@ from locust import HttpUser, TaskSet, task, between
 import os
 
 user = os.environ.get('GITHUB_USERNAME')
+testVar = "ayyyyyy"
 print("GITHUB_USERNAME = ", os.environ.get('GITHUB_USERNAME'))
 print("user = ", user)
+print("testVar = ", testVar)
 
 class UserTasks(TaskSet):
 	@task(1)
