704cc2b7e013d8446920b0d3b258359500c32cb1
apardo04 <apardo04@gmail.com>
2022-11-25 13:34:32 -0500

    testing test.yml

--- locustfile.py
+++ locustfile.py
@@ -2,10 +2,17 @@ from locust import HttpUser, TaskSet, task, between
 import os
 
 user = os.environ.get('GITHUB_USERNAME')
+user2 = os.environ['GITHUB_USERNAME']
 testVar = "ayyyyyy"
 print("GITHUB_USERNAME = ", os.environ.get('GITHUB_USERNAME'))
 print("user = ", user)
+print("user2 = ", user2)
 print("testVar = ", testVar)
+
+if os.getenv('GITHUB_USERNAME'):
+    print('Looks like GitHub!')
+else:
+    print('Maybe running locally?')
 
 class UserTasks(TaskSet):
 	@task(1)
