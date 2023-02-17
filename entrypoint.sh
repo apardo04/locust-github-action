02be48c598decc8bf2f1963c04ba574907839018
apardo04 <apardo04@gmail.com>
2023-02-16 23:16:20 -0500

    initial requirements branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,10 +10,8 @@ fi
 if [ -z "$INPUT_REQUIREMENTS" ]
 then
     pip install -r requirements.txt
-    echo "ayy In the then requirements IF"
 else
     pip install -r /github/workspace/$INPUT_REQUIREMENTS
-    echo "ayy in the else requirements IF"
 fi
 
 locust -f $file --headless -u $INPUT_USERS -r $INPUT_RATE --run-time $INPUT_RUNTIME -H $INPUT_URL
--- locustfile.py
+++ locustfile.py
@@ -1,17 +1,4 @@
 from locust import HttpUser, TaskSet, task, between
-import flask
-import requests
-import pandas as pd
-
-print("pandas version:")
-print(pd.__version__)
-
-response = requests.get("https://www.google.com")
-print("requests status code:")
-print(response.status_code)
-
-print("Flask Version:")
-print(flask.__version__)
 
 class UserTasks(TaskSet):
 	@task(1)
